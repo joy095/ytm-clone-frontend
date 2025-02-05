e82ab0cb3778a40d2e97e2beb2722ace4be8f86e
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-05 18:32:05 +0530

    added out dir

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-      }
+      },
     ],
   },
+  distDir: "out",
 };
 
 export default nextConfig;
